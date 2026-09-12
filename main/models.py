b8ec08f892a86fd1ee2ae215d48eff446ba7430e
Salsabilla Hasan <salsabilla.hasan@ui.ac.id>
2026-09-12 22:45:35 +0700

    Implement dynamic experience section using MVT

--- main/models.py
+++ main/models.py
@@ -13,11 +13,12 @@ class Experience(models.Model):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(blank=True, null=True)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
     
